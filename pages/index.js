ebe86b44636c8049c96abc767d04980d72c199bb
Daramfon Ukpong <kandysfirst@gmail.com>
2023-03-04 14:44:36 +0100

    Fixed linting

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Image from "next/image";
+
 import styles from "@/styles/Home.module.css";
 import Navbar from "@/components/navbar";
 import Hero from "@/components/hero";
